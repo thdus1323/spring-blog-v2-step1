ddffb981dcc0dd998d57e0feef498bc4af2cc7db
thdus1323 <thdus1323@naver.com>
2024-03-19 20:45:37 +0900

    익명게시판 board 완성

--- src/main/java/shop/mtcoding/blog/BlogApplication.java
+++ src/main/java/shop/mtcoding/blog/BlogApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class git BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
